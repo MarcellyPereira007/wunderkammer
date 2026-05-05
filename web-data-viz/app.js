6624de5141209ca5b0918743f651bb48cf3ce9e3
MarcellyPereira007 <marcelly.silva@sptech.school>
2026-05-04 22:10:43 -0300

    incluindo router pra dash no app js

--- web-data-viz/app.js
+++ web-data-viz/app.js
@@ -16,6 +16,8 @@ var app = express();
 var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var categoriaRouter = require("./src/routes/categorias");
+var dashboardRouter = require("./routes/dashboard");
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -26,6 +28,8 @@ app.use(cors());
 app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/categorias", categoriaRouter);
+app.use("/dashboard", dashboardRouter);
+
 app.listen(PORTA_APP, function () {
     console.log(`
     ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
